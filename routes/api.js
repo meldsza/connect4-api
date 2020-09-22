51e5d72b4d329703220e62d467db0e3d912e3753
Melroy Neil Dsouza <meldsza@gmail.com>
2020-09-22 16:53:20 +0530

    add readme to /

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,18 @@
+const { response } = require('express');
 const express = require('express');
 let router = express.Router();
+let path = require('path');
 
 //For the sake of convenience, I have made these as get routes. Normally these should
 //be post routes as per REST standards
 
 //route to start game
-router.get('/start', require('../src/controllers/connect4Controller').start);
+router.all('/start', require('../src/controllers/connect4Controller').start);
 
 //route to make a move
-router.get('/move', require('../src/controllers/connect4Controller').move);
+router.all('/move', require('../src/controllers/connect4Controller').move);
+router.all('/', (req, res) => {
+    res.sendFile(path.resolve('./README.txt'));
+});
 
 module.exports = router
